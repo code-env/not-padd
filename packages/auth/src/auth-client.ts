7c839725a036c7fd0f394a17232ff111270828d8
code-env <org.codeen@gmail.com>
2025-09-13 21:09:04 +0100

    feat: added authentication

--- packages/auth/src/auth-client.ts
+++ packages/auth/src/auth-client.ts
@@ -1,5 +1,6 @@
 import { createAuthClient } from "better-auth/react";
+import { env } from "@notpadd/env/client";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: env.NEXT_PUBLIC_BACKEND_URL,
 });
--- packages/auth/src/auth.ts
+++ packages/auth/src/auth.ts
@@ -2,21 +2,10 @@ import { db } from "@notpadd/db";
 import schema from "@notpadd/db/schema";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-
-if (!process.env.FRONTEND_URL || !process.env.BACKEND_URL) {
-  throw new Error(
-    "Missing environment variables. FRONTEND_URL or BACKEND_URL is not defined"
-  );
-}
-
-if (!process.env.BETTER_AUTH_SECRET) {
-  throw new Error(
-    "Missing BETTER_AUTH_SECRET in environment variables. Please set it in your .env file."
-  );
-}
+import { env } from "@notpadd/env/server";
 
 export const auth = betterAuth({
-  secret: process.env.BETTER_AUTH_SECRET,
+  secret: env.BETTER_AUTH_SECRET,
   database: drizzleAdapter(db, {
     provider: "pg",
     schema: {
@@ -24,7 +13,7 @@ export const auth = betterAuth({
     },
   }),
 
-  trustedOrigins: [process.env.FRONTEND_URL, process.env.BACKEND_URL],
+  trustedOrigins: [env.FRONTEND_URL, env.BACKEND_URL],
 
   emailAndPassword: {
     enabled: false,
@@ -32,8 +21,8 @@ export const auth = betterAuth({
 
   socialProviders: {
     google: {
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
       scope: [
         "https://www.googleapis.com/auth/userinfo.profile",
         "https://www.googleapis.com/auth/userinfo.email",
