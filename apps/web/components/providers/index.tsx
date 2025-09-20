6e39e19b98570c3e100931c3a709fc925ea47a7d
code-env <org.codeen@gmail.com>
2025-09-20 10:54:22 +0100

    Add use-mounted hook and update providers
    - Add new use-mounted.ts hook for component mounting state
    - Update providers/index.tsx component

--- apps/web/components/providers/index.tsx
+++ apps/web/components/providers/index.tsx
@@ -5,18 +5,24 @@ import { Toaster } from "sonner";
 import { OrganizationProvider } from "@/contexts";
 import { authClient } from "@notpadd/auth/auth-client";
 import { usePathname, useRouter } from "next/navigation";
+import { useMounted } from "@/hooks/use-mouted";
 
 const Providers = ({ children }: { children: ReactNode }) => {
-  const { data: activeOrganization } = authClient.useActiveOrganization();
+  const { data: activeOrganization, isPending } =
+    authClient.useActiveOrganization();
   const router = useRouter();
   const pathname = usePathname();
+  const mounted = useMounted();
 
   useEffect(() => {
+    if (!mounted) return;
+
     if (pathname === "/") {
       router.push(`/${activeOrganization?.slug}`);
     }
-  }, [activeOrganization]);
+  }, [activeOrganization, mounted]);
 
+  if (!mounted || isPending) return null;
   return (
     <OrganizationProvider>
       {children}
