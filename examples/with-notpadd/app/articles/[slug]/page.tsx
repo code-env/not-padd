22f0ebacdf3db9da1bd8df5e85e5ac9005196e20
code-env <org.codeen@gmail.com>
2025-10-28 19:01:22 +0100

    feat: removed the dark mode on the app

--- examples/with-notpadd/app/articles/[slug]/page.tsx
+++ examples/with-notpadd/app/articles/[slug]/page.tsx
@@ -16,7 +16,7 @@ export default async function Article({ params }: Props) {
   }
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center gap-20  py-32 px-16 sm:items-start">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center gap-20  py-32 px-16 sm:items-start bg-white">
         <article className="prose space-y-6">
           <h1 className="text-4xl mb-14 font-medium font-mono first-letter:capitalize">
             {post.title}
--- examples/with-notpadd/app/page.tsx
+++ examples/with-notpadd/app/page.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center gap-20  py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1>Notpadd Next</h1>
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center gap-20  py-32 px-16 bg-white  sm:items-start">
+        <h1 className="text-4xl font-medium font-mono">Notpadd Next example</h1>
         <ul className="flex flex-col w-full">
           {allPosts.map((post) => (
             <li key={post.slug} className="w-full">
