7d9285bf75ba7611e08785156bc6d4ece5afc4fe
code-env <org.codeen@gmail.com>
2025-11-25 09:56:03 +0100

    feat: added the docs button

---
+++ apps/web/components/docs-button.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Button } from "@notpadd/ui/components/button";
+import { Book } from "lucide-react";
+
+export const DocsButton = () => {
+  return (
+    <Button
+      variant="outline"
+      size="sm"
+      onClick={() => {
+        window.open("https://docs.notpadd.com", "_blank");
+      }}
+    >
+      <Book className="size-4" />
+      Docs
+    </Button>
+  );
+};
